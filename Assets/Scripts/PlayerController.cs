bda39544c5d291339688eddfa3f5a826eb0a564f
이정욱 <ljw@wemakeprice.com>
2024-06-05 10:46:03 +0900

    character movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 3.0f;
+    void Start()
+    {
+        
+    }
+
+    // GameObject (Player)
+        // Transform
+        // PlayerController (*)
+        // Transform 의 위치를 변경시켜야 하는데 자주 사용하기 때문에 transform 바로 사용
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+
+        //transform   
+    }
+}
