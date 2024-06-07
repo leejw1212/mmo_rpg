bc9c22a9b5f1f312fca8a67d2eb1aaaa974b04f5
이정욱 <ljw@wemakeprice.com>
2024-06-07 14:39:38 +0900

    Ray Casting

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -23,19 +23,42 @@ public class TestCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        //Vector3 look = transform.TransformDirection(Vector3.forward);
+        //Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
 
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position + Vector3.up, look, out hit, 10))
-            Debug.Log($"RayCast @ {hit.collider.gameObject.name} !");
+        //RaycastHit hit;
+        //if (Physics.Raycast(transform.position + Vector3.up, look, out hit, 10))
+        //    Debug.Log($"RayCast @ {hit.collider.gameObject.name} !");
 
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+        //RaycastHit[] hits;
+        //hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
 
-        foreach (RaycastHit hitt in hits)
+        //foreach (RaycastHit hitt in hits)
+        //{
+        //    Debug.Log($"RayCastAll @ {hitt.collider.gameObject.name} !");
+        //}
+
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log($"RayCastAll @ {hitt.collider.gameObject.name} !");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            //Vector3 dir = mousePos - Camera.main.transform.position;
+            //dir = dir.normalized;
+            // same ray
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+            // Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+
+            RaycastHit hit;
+
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+            //int mask = (1 << 8) | (1 << 9);
+
+            if (Physics.Raycast(ray, out hit, 100.0f, mask))
+            // if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
         }
     }
 }
