47f53517db4d634d09b226716378971fd64ad04b
leejw1212 <jwleekw@gmail.com>
2024-06-11 00:33:10 +0900

    last

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -7,6 +7,8 @@ public class Managers : MonoBehaviour
     static Managers s_instance;
     public static Managers Instance { get { Init(); return s_instance;} }
 
+
+    DataManager _data = new DataManager();
     InputManager _input = new InputManager();
     PoolManager _pool = new PoolManager();
     ResourceManager _resource = new ResourceManager();
@@ -14,6 +16,7 @@ public class Managers : MonoBehaviour
     UIManager _ui = new UIManager();
     SoundManager _sound = new SoundManager();
 
+    public static DataManager Data { get { return Instance._data; } }
     public static InputManager Input { get { return Instance._input; } }
     public static PoolManager Pool { get { return Instance._pool; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
@@ -49,6 +52,7 @@ public class Managers : MonoBehaviour
 
             s_instance._pool.Init();
             s_instance._sound.Init();
+            s_instance._data.Init();
         }
     }
 
